67bcfd95a2c9b9fac73f213fd595d0737ee822a0
Thomas Asanger <thomas.asanger@1und1.de>
2017-09-01 17:40:11 +0200

    updated code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
+public class hw
+{
+	public static void main(String args[])
+	{
+	   System.out.println("Hello World");
+	}
 }
